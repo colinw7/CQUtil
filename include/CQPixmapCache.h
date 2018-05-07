ca95e21410ff488832c12ef78ce4a29a3db5fd9f
Colin Williams <colinw@nc.rr.com>
2018-05-07 17:37:14 -0400

    new files

--- include/CQPixmapCache.h
+++ include/CQPixmapCache.h
@@ -12,11 +12,23 @@ class CQPixmapCache {
  public:
   static CQPixmapCache *instance();
 
+  static void release();
+
+ public:
+ ~CQPixmapCache();
+
+  bool isDark() const { return dark_; }
+  void setDark(bool b) { dark_ = b; }
+
+  void clear();
+
   void addData(const QString &id, const uchar *data, int len);
 
   const QPixmap &getPixmap(const QString &id);
 
-  QIcon getIcon(const QString &id);
+  QIcon getIcon(const QString &lightId, const QString &darkId="");
+
+  QIcon getPixmapIcon(const QString &id);
 
   QPixmap getSizedPixmap(const QString &id, const QSize &s);
 
@@ -32,11 +44,16 @@ class CQPixmapCache {
     Data(const uchar *data1=0, int len1=0) :
      data(data1), len(len1) {
     }
+
+   ~Data() {
+      delete pixmap;
+    }
   };
 
   typedef std::map<QString,Data> IdData;
 
   IdData idData_;
+  bool   dark_ { false };
 };
 
 #endif
